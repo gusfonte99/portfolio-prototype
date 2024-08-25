fd180e5d36d9847f9e9b7beef699fa63349f2c56
Gustavo Fonte <gusfonte99@gmail.com>
2024-08-25 21:09:02 +0100

    Added About component

---
+++ portfolio-prototype/components/about.jsx
@@ -0,0 +1,44 @@
+export default function About() {
+  return (
+    <section className="flex flex-col justify-center items-center mt-20">
+      <h2 className="text-3xl font-semibold">About me</h2>
+
+      <ul className="flex flex-col gap-3 mt-6">
+        <li>
+          <p className="text-lg text-center font-normal">
+            After over 2 years of working in retail, looking for a way to break
+            into the <strong>Tech</strong> field, I embarked on a new adventure.
+          </p>
+        </li>
+        <li>
+          <p className="text-lg text-center font-normal">
+            My fascination with programming led me to shift gears and pursue web
+            development. <strong>Problem-solving</strong> and continuous <strong>learning</strong> have always
+            been my passions, and they drive me to excel in this dynamic field.
+          </p>
+        </li>
+        <li>
+          <p className="text-lg text-center font-normal">
+            My core stack <strong>{"(MERN)"}</strong> includes{" "}
+            <strong>MongoDB</strong>, <strong>Express.js</strong>,{" "}
+            <strong>React</strong>, and <strong>Node.js</strong>. I'm also
+            proficient with <strong>TypeScript</strong> and have a passion for
+            exploring new technologies and frameworks. I'm actively seeking a
+            full-time role as a <strong>Full Stack Developer</strong>, but I'm
+            open to flexible opportunities that align with my{" "}
+            <strong>skills</strong> and <strong>enthusiasm</strong> for growth.
+          </p>
+        </li>
+        <li>
+          <p className="text-lg text-center font-normal">
+            When I'm not in front of my computer, I love to{" "}
+            <strong>play games</strong>, <strong>volleyball</strong>,{" "}
+            <strong>guitar</strong>, and learn new <strong>languages</strong> or{" "}
+            <strong>cook</strong> new recipes. My <strong>passion</strong> for
+            learning extends beyond the digital world.
+          </p>
+        </li>
+      </ul>
+    </section>
+  );
+}
--- portfolio-prototype/src/app/page.tsx
+++ portfolio-prototype/src/app/page.tsx
@@ -1,9 +1,12 @@
 import Header from "../../components/header";
+import About from "../../components/about";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between w-full min-h-screen pt-20 px-6">
+    <main className="flex flex-col items-center justify-start w-full min-h-screen pt-20 px-6">
       <Header />
+
+      <About />
     </main>
   );
 }
