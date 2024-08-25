fc007204a0dc6f57327970619bf0c211dae39ef1
Gustavo Fonte <gusfonte99@gmail.com>
2024-08-25 23:03:27 +0100

    Updated styling

--- portfolio-prototype/components/footer.tsx
+++ portfolio-prototype/components/footer.tsx
@@ -1,8 +1,13 @@
-export default function Footer () {
+export default function Footer() {
   return (
     <footer className="flex flex-col justify-center items-center gap-2 mt-16 mb-10">
       <small>© 2024 Gustavo Fonte. All rights reserved</small>
-      <small className="text-center">About this website: built with React & Next.js(App Router & Server Actions), Typescript, Tailwind CSS, React Email & Resend, Vercel hosting.</small>
+      
+      <small className="text-center">
+        About this website: built with React & Next.js(App Router & Server
+        Actions), Typescript, Tailwind CSS, React Email & Resend, Vercel
+        hosting.
+      </small>
     </footer>
-  )
+  );
 }
--- portfolio-prototype/components/projects.tsx
+++ portfolio-prototype/components/projects.tsx
@@ -1,31 +1,34 @@
-export default function Projects () {
+export default function Projects() {
   return (
     <section className="flex flex-col justify-center items-center w-full mt-20">
       <h2 className="text-2xl font-semibold sm:text-3xl">My Projects</h2>
 
-      <ul className="flex flex-col justify-center items-center gap-6 w-full mt-8">
-        <li className="grid grid-cols-1 grid-rows-1 w-full pt-4 px-5 pb-7 rounded-lg bg-slate-400">
-          <div className="">
+      <ul className="flex flex-col justify-center items-center gap-6 w-full mt-8 sm:max-w-2xl">
+        <li className="group grid grid-cols-1 grid-rows-1 w-full rounded-lg bg-slate-800/70 overflow-hidden sm:grid-cols-2 hover:bg-slate-800">
+          <div className="pt-4 pl-5 pr-5 pb-7 sm:pl-10 sm:pt-10 sm:pr-2">
             <h3 className="text-2xl font-semibold">Aniki</h3>
 
-            <p className="mt-2">An anime forum Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis error obcaecati.</p>
+            <p className="font-medium mt-2">
+              An anime forum Lorem ipsum dolor sit amet consectetur adipisicing
+              elit. Officiis error obcaecati.
+            </p>
 
             <div className="mt-4">
               <ul className="flex flex-row flex-wrap gap-2">
-                <li className="text-sm font-medium px-3 rounded-full bg-slate-900">React</li>
-                <li className="text-sm font-medium px-3 rounded-full bg-slate-900">React</li>
-                <li className="text-sm font-medium px-3 rounded-full bg-slate-900">React</li>
-                <li className="text-sm font-medium px-3 rounded-full bg-slate-900">React</li>
-                <li className="text-sm font-medium px-3 rounded-full bg-slate-900">React</li>
+                <li className="text-sm text-neutral-300 leading-6 font-medium px-3 rounded-full bg-zinc-950">React</li>
+                <li className="text-sm text-neutral-300 leading-6 font-medium px-3 rounded-full bg-zinc-950">React</li>
+                <li className="text-sm text-neutral-300 leading-6 font-medium px-3 rounded-full bg-zinc-950">React</li>
+                <li className="text-sm text-neutral-300 leading-6 font-medium px-3 rounded-full bg-zinc-950">React</li>
+                <li className="text-sm text-neutral-300 leading-6 font-medium px-3 rounded-full bg-zinc-950">React</li>
               </ul>
             </div>
           </div>
 
-          <div className="hidden relative overflow-hidden">
-            <div className="w-[28rem] h-56 bg-slate-50"></div>
+          <div className="hidden relative sm:inline">
+            <div className="image absolute -right-12 top-8 w-80 h-40 rounded-t-lg shadow-2xl bg-slate-50 transition group-hover:scale-x-105 group-hover:-translate-x-5 group-hover:translate-y-2 group-hover:-rotate-2"></div>
           </div>
         </li>
       </ul>
     </section>
-  )
+  );
 }
