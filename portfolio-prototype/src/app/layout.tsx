1ec83cda54b0ed823db99dbed3fa47090ca572e3
Gustavo Fonte <gusfonte99@gmail.com>
2024-08-27 19:18:50 +0100

    Extracted Navbar and Footer to layout

--- portfolio-prototype/src/app/layout.tsx
+++ portfolio-prototype/src/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Inter_Tight } from "next/font/google";
 import "./globals.css";
 
+import Navbar from "../../components/navbar";
+import Footer from "../../components/footer";
+
 const inter = Inter_Tight({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -9,12 +12,14 @@ export const metadata: Metadata = {
   description: "Gustavo's Portfolio",
 };
 
-export default function RootLayout({children}: Readonly<{children: React.ReactNode}>) {
-
+export default function RootLayout({children}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className="!scroll-smooth">
       <body className={`${inter.className} text-white bg-zinc-950`}>
+        <Navbar />
         {children}
+        {/* <Toaster position="top-right" /> */}
+        <Footer />
       </body>
     </html>
   );
