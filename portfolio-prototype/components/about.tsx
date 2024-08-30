7e552015434d7b5e4b0eada6a94d1c44db84feaa
Gustavo Fonte <gusfonte99@gmail.com>
2024-08-30 23:29:17 +0100

    Updated About css

--- portfolio-prototype/components/about.tsx
+++ portfolio-prototype/components/about.tsx
@@ -1,44 +1,54 @@
 export default function About() {
-  return (
-    <section className="flex flex-col justify-center items-center max-w-4xl mt-20">
-      <h2 className="text-2xl font-semibold sm:text-3xl">About me</h2>
 
-      <ul className="flex flex-col gap-3 mt-6">
-        <li>
-          <p className="text-base text-center font-normal sm:text-lg">
-            After over 2 years of working in retail, looking for a way to break
-            into the <strong>Tech</strong> field, I embarked on a new adventure.
-          </p>
-        </li>
-        <li>
-          <p className="text-base text-center font-normal sm:text-lg">
-            My fascination with programming led me to shift gears and pursue web
-            development. <strong>Problem-solving</strong> and continuous <strong>learning</strong> have always
-            been my passions, and they drive me to excel in this dynamic field.
-          </p>
-        </li>
-        <li>
-          <p className="text-base text-center font-normal sm:text-lg">
-            My core stack <strong>{"(MERN)"}</strong> includes{" "}
-            <strong>MongoDB</strong>, <strong>Express.js</strong>,{" "}
-            <strong>React</strong>, and <strong>Node.js</strong>. I'm also
-            proficient with <strong>TypeScript</strong> and have a passion for
-            exploring new technologies and frameworks. I'm actively seeking a
-            full-time role as a <strong>Full Stack Developer</strong>, but I'm
-            open to flexible opportunities that align with my{" "}
-            <strong>skills</strong> and <strong>enthusiasm</strong> for growth.
-          </p>
-        </li>
-        <li>
-          <p className="text-base text-center font-normal sm:text-lg">
-            When I'm not in front of my computer, I love to{" "}
-            <strong>play games</strong>, <strong>volleyball</strong>,{" "}
-            <strong>guitar</strong>, and learn new <strong>languages</strong> or{" "}
-            <strong>cook</strong> new recipes. My <strong>passion</strong> for
-            learning extends beyond the digital world.
-          </p>
-        </li>
-      </ul>
+  return (
+    <section id="about" className="flex flex-col justify-center items-center w-full pl-52 my-20">
+      <h2 className="text-2xl font-semibold sm:text-5xl">A little bit about me..</h2>
+
+        <ul className="flex flex-col gap-10 mt-20">
+          <li className="place-self-start flex flex-row justify-center items-center gap-8 w-2/3 px-8 py-6 rounded-r-full bg-zinc-900">
+            <p className="text-base text-pretty font-normal sm:text-lg">
+              After over 2 years of working in retail, looking for a way to break
+              into the <strong className="font-semibold text-indigo-300">Tech</strong> field, I embarked on a new adventure.
+              My fascination with programming led me to shift gears and pursue web
+              development. <strong className="font-semibold text-indigo-300">Problem-solving</strong> and continuous{" "}
+              <strong className="font-semibold text-indigo-300">learning</strong> have always been my passions, and they
+              drive me to excel in this dynamic field.
+            </p>
+            
+            <div className="basis-24 shrink-0">
+              <svg className="fill-indigo-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M481.12-270.77q13.26 0 22.38-9.16t9.12-22.42q0-13.27-9.16-22.38-9.16-9.12-22.43-9.12-13.26 0-22.38 9.16-9.11 9.16-9.11 22.43 0 13.26 9.16 22.38 9.16 9.11 22.42 9.11ZM480.13-120q-74.67 0-140.41-28.34-65.73-28.34-114.36-76.92-48.63-48.58-76.99-114.26Q120-405.19 120-479.87q0-74.67 28.34-140.41 28.34-65.73 76.92-114.36 48.58-48.63 114.26-76.99Q405.19-840 479.87-840q74.67 0 140.41 28.34 65.73 28.34 114.36 76.92 48.63 48.58 76.99 114.26Q840-554.81 840-480.13q0 74.67-28.34 140.41-28.34 65.73-76.92 114.36-48.58 48.63-114.26 76.99Q554.81-120 480.13-120Zm-.13-40q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Zm2.48-170.46q30.37 0 52.33 18.86 21.96 18.85 21.96 47.14 0 22-12.35 39.77-12.34 17.77-28.57 32.77-20.56 18.64-36.21 41.01-15.64 22.37-17.95 49.29-.77 7.85 4.89 13.12 5.65 5.27 13.19 5.27 8.08 0 13.58-5.39 5.5-5.38 6.96-12.69 4-20.23 16.07-35.96 12.08-15.73 26.5-29.79 20.74-20.63 36.85-44.97 16.12-24.33 16.12-54.28 0-44.07-33.43-72.73Q529-687.69 484-687.69q-33.38 0-62.12 15.61-28.73 15.62-45.96 43.23-3.92 6.62-2.51 13.69 1.41 7.07 7.62 10.74 7.89 4.19 15.97 1.96 8.08-2.23 14.23-9.31 13.31-16.54 31.35-27.61 18.04-11.08 39.9-11.08Z"/></svg>
+            </div>
+          </li>
+          <li className="place-self-end flex flex-row justify-center items-center gap-8 w-2/3 px-8 py-6 rounded-l-full bg-zinc-900">
+            <div className="basis-24 shrink-0">
+              <svg className="fill-indigo-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M184.62-200q-27.62 0-46.12-18.5Q120-237 120-264.62v-430.76q0-27.62 18.5-46.12Q157-760 184.62-760h590.76q27.62 0 46.12 18.5Q840-723 840-695.38v430.76q0 27.62-18.5 46.12Q803-200 775.38-200H184.62Zm0-40h590.76q9.24 0 16.93-7.69 7.69-7.69 7.69-16.93V-640H160v375.38q0 9.24 7.69 16.93 7.69 7.69 16.93 7.69Zm190.84-200-89.84-89.85q-5.85-5.84-6.12-14.15-.27-8.31 6.35-14.15 5.84-5.62 14.15-5.35 8.31.27 14.15 6.12l94.77 94.76q9.7 9.7 9.7 22.62 0 12.92-9.7 22.62l-95.54 95.53q-5.61 5.62-13.65 5.35-8.04-.27-13.88-6.12-5.62-5.61-5.62-13.38 0-7.77 5.62-13.38L375.46-440ZM520-300q-8.54 0-14.27-5.73T500-320q0-8.54 5.73-14.27T520-340h160q8.54 0 14.27 5.73T700-320q0 8.54-5.73 14.27T680-300H520Z"/></svg>
+            </div>
+            
+            <p className="text-base text-pretty font-normal sm:text-lg">
+              My core stack {"( "}<strong className="font-semibold text-indigo-300">{"MERN"}</strong>{" )"} includes{" "}
+              <strong className="font-semibold text-indigo-300">MongoDB</strong>, <strong className="font-semibold text-indigo-300">Express.js</strong>,{" "}
+              <strong className="font-semibold text-indigo-300">React</strong>, and <strong className="font-semibold text-indigo-300">Node.js</strong>. I'm also
+              proficient with <strong className="font-semibold text-indigo-300">TypeScript</strong> and have a passion for
+              exploring new technologies and frameworks. I'm actively seeking a
+              full-time role as a <strong className="font-semibold text-indigo-300">Full Stack Developer</strong>, but I'm
+              open to flexible opportunities that align with my{" "}
+              <strong className="font-semibold text-indigo-300">skills</strong> and <strong className="font-semibold text-indigo-300">enthusiasm</strong> for growth.
+            </p>
+          </li>
+          <li className="place-self-start flex flex-row justify-center items-center gap-8 w-2/3 px-8 py-6 rounded-r-full bg-zinc-900">
+            <p className="text-base text-pretty font-normal sm:text-lg">
+              When I'm not in front of my computer, I love to play{" "}
+              <strong className="font-semibold text-indigo-300">games</strong>, <strong className="font-semibold text-indigo-300">volleyball</strong>,{" "}
+              <strong className="font-semibold text-indigo-300">guitar</strong>, and learn new <strong className="font-semibold text-indigo-300">languages</strong> or{" "}
+              <strong className="font-semibold text-indigo-300">cook</strong> new recipes. My <strong className="font-semibold text-indigo-300">passion</strong> for
+              learning extends beyond the digital world.
+            </p>
+            
+            <div className="basis-24 shrink-0">
+              <svg className="fill-indigo-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M479.23-196.23q-11.69 0-23.11-4.23-11.43-4.23-20.12-12.92l-45.92-41.47q-106.77-97-188.43-188.65Q120-535.15 120-634q0-76.31 51.85-128.15Q223.69-814 300-814q43.77 0 91.15 22.12 47.39 22.11 88.85 83.42 41.46-61.31 88.85-83.42Q616.23-814 660-814q76.31 0 128.15 51.85Q840-710.31 840-634q0 101.15-85 194.46T568.92-254.62l-45.69 41.24q-8.69 8.69-20.5 12.92t-23.5 4.23Zm-18.77-470.69q-35.92-57.93-74.69-82.5Q347-774 300-774q-60 0-100 40t-40 100q0 44.31 25.08 90.88 25.07 46.58 67.34 95.43 42.27 48.84 97.16 99.92 54.88 51.08 114.27 105.15 6.92 6.16 16.15 6.16 9.23 0 16.15-6.16 59.39-54.07 114.27-105.15 54.89-51.08 97.16-99.92 42.27-48.85 67.34-95.43Q800-589.69 800-634q0-60-40-100t-100-40q-47 0-85.77 24.58-38.77 24.57-74.69 82.5-3.16 5.38-8.54 8.07-5.38 2.7-11 2.7t-11-2.7q-5.38-2.69-8.54-8.07ZM480-504.08Z"/></svg>
+            </div>
+          </li>
+        </ul>
     </section>
   );
 }
