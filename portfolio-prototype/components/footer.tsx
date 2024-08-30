8adcf189772c0ce12c89361ab3ba7daa5752d888
Gustavo Fonte <gusfonte99@gmail.com>
2024-08-30 23:33:28 +0100

    Adjusted Footer placement

--- portfolio-prototype/components/footer.tsx
+++ portfolio-prototype/components/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="flex flex-col justify-center items-center gap-2 mt-16 mb-10">
+    <footer className="flex flex-col justify-center items-center gap-2 mt-16 pl-52 mb-10">
       <small>© 2024 Gustavo Fonte. All rights reserved</small>
       
       <small className="text-center">
