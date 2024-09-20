de82bb532b996976e20e95b817eba7a8894a505c
Gustavo Fonte <gusfonte99@gmail.com>
2024-09-20 22:42:44 +0100

    Type declaration for third-party library - boxicons

---
+++ src/app/types/boxicons.d.ts
@@ -0,0 +1,14 @@
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      "box-icon": IconProps;
+    }
+  }
+}
+
+export type IconProps = {
+  type: "regular" | "solid" | "logo";
+  name: string;
+  size?: string;
+  color?: string;
+};
