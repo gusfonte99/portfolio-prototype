e1de321d7d80648d8578925af4c5488222044297
Gustavo Fonte <gusfonte99@gmail.com>
2024-10-01 04:06:06 +0100

    Updated component structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Chakra_Petch } from "next/font/google";
 import "./globals.css";
 
 import Footer from "../../components/footer";
-import ScrollHint from "../../components/scroll-hint";
 
 const chakra = Chakra_Petch({
   weight: ["300", "400", "500", "600", "700"],
@@ -20,7 +19,6 @@ export default function RootLayout({children}: Readonly<{ children: React.ReactN
     <html lang="en" className="!scroll-smooth">
       <body className={`${chakra.className} text-white bg-zinc-950`}>
         {children}
-        <ScrollHint />
         <Footer />
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,21 +4,30 @@ import About from "../../components/about";
 import Projects from "../../components/projects";
 import Experience from "../../components/experience";
 import Skills from "../../components/skills";
+import Test from "../../components/test";
+import ScrollHint from "../../components/scroll-hint";
+import { ActiveSectionProviderWrapper } from "../../context/active-section.context";
 
 export default function Home() {
   return (
     <main className="flex flex-col w-full min-h-screen">
-      <Navbar />
+      <ActiveSectionProviderWrapper>
+        <Navbar />
 
-      <Header />
+        <ScrollHint />
 
-      <Skills />
+        <Header />
 
-      <About />
+        <Test />
 
-      <Projects />
+        <Skills />
 
-      <Experience />
+        <About />
+
+        <Projects />
+
+        <Experience />
+      </ActiveSectionProviderWrapper>
     </main>
   );
 }
