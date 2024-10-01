f1f18b7bf35e8e167edc917d150a202f5022c60f
Gustavo Fonte <gusfonte99@gmail.com>
2024-10-01 04:01:56 +0100

    Updated Navbar and Link components to work with context API

--- components/link.tsx
+++ components/link.tsx
@@ -1,15 +1,15 @@
 import { SectionHash, SectionName, type Link } from "../lib/types"
 
 type LinkProps = Link & {
-  active: boolean
+  isActive: boolean
   onClick: (sectionName: SectionName, sectionHash: SectionHash, offset?: number) => void
 }
 
-export default function Link ({ name, hash, active, onClick }: LinkProps) {
+export default function Link ({ name, hash, isActive, onClick }: LinkProps) {
 
   return (
     <li>
-      <button data-active={active} onClick={() => {onClick(name, hash)}} className="w-full h-full py-3 data-[active=true]:text-white data-[active=true]:font-medium">
+      <button data-active={isActive} onClick={() => {onClick(name, hash)}} className="w-full h-full py-3 data-[active=true]:text-white data-[active=true]:font-medium">
         {name}
       </button>
     </li>
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,16 +1,29 @@
 'use client'
 
-import styles from "@/app/styles/logo.module.css"
-import { Chakra_Petch } from "next/font/google";
+import React from "react";
 
-import Link from "next/link";
 import { links } from "../lib/data";
+import { SectionHash, SectionName } from "../lib/types";
+
+import { useActiveSectionContext } from "../context/active-section.context";
+
 import Icon from "./icon";
+import Link from "./link";
 
 export default function Navbar () {
+  
+  const { activeSection, setActiveSection } = useActiveSectionContext()
 
-  const scrollIntoViewWithOffset = (section: string, offset: number = 60) : void => {
-    const selectedSection = document.querySelector(section) as HTMLElement
+  // Scroll to selected section with an offset
+  // Edit offset value as needed
+  const scrollIntoViewWithOffset = (sectionName: SectionName, sectionHash: SectionHash, offset: number = 160) : void => {
+    const selectedSection = document.querySelector(sectionHash)
+    
+    if (!selectedSection) {
+      throw new Error ("Selected section not found.")
+    }
+
+    setActiveSection(sectionName)
 
     window.scrollTo({
       top:
@@ -21,7 +34,7 @@ export default function Navbar () {
 
   return(
     <nav className="z-[999] fixed left-0 w-52 h-full bg-neutral-950 border-r border-neutral-600/60">
-      <div className="flex flex-col justify-start items-center w-full pt-10 pb-6 select-none">
+      <div className="flex flex-col justify-start items-center w-full py-10 select-none">
         <div className="flex flex-row justify-center items-center gap-2 w-full">
           <span className="text-8xl font-semibold">g</span>
           <div className="w-10 h-28 bg-white"></div>
@@ -31,14 +44,12 @@ export default function Navbar () {
         <span className="text-base text-neutral-400 mt-3">Fullstack Developer</span>
       </div>
       
-      <ul className="mt-12 text-center text-neutral-400 text-lg font-light border-t-[1px] border-neutral-600/60 *:border-b-[1px] *:border-neutral-600/60">
+      <ul className="text-center text-neutral-400 text-lg font-light border-t-[1px] border-neutral-600/60 *:border-b-[1px] *:border-neutral-600/60">
         {links.map((link, index) => {
           return (
-            <li key={index}>
-              <button onClick={() => scrollIntoViewWithOffset(link.hash)} className="w-full h-full py-3">
-                {link.name}
-              </button>
-            </li>
+            <React.Fragment key={index}>
+              <Link {...link} isActive={link.name === activeSection ? true : false} onClick={scrollIntoViewWithOffset} />
+            </React.Fragment>
           )
         })}
       </ul>
